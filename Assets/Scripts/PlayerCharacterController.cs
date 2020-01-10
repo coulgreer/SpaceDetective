a9c6fa1c72871426052a46f7685f115af81cf4e9
Coul Greer <coulgreer1@hotmail.com>
2020-01-10 11:03:56 -0500

    Added the basics of movement.
    Started the staging area to test the mechanics of the game. Started with the background, camera movement, and character movement.

---
+++ Assets/Scripts/PlayerCharacterController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacterController : MonoBehaviour
+{
+    public float speed = 3.0f;
+
+    private Rigidbody2D rigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePosition();
+    }
+
+    private void UpdatePosition() {
+        float horizontalDelta = Input.GetAxis("Horizontal");
+
+        Vector2 move = new Vector2(horizontalDelta, 0);
+
+        Vector2 position = rigidbody2D.position;
+        position += move * speed * Time.deltaTime;
+        rigidbody2D.MovePosition(position);
+    }
+}
